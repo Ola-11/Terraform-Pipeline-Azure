98372531bfc9b733c9f39e31cb58b25c336db323
Oladeinde Oluwaseun <90369121+Ola-11@users.noreply.github.com>
2024-11-06 15:52:43 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,10 +13,13 @@ terraform {
     }
   }
   backend "azurerm" {
-      resource_group_name  = "rg-soladeinde-training-cc-001"
+      resource_group_name  = "RG-Project10"
       storage_account_name = "inwayztfstate1248310649"
       container_name       = "inwayztfstate"
       key                  = "d905iyQFg6g2/lExrzOT7kMiektbBXGPGtwKRHwYI6qw04/qFYasGKxYFSWtpRwj7ix+b0eBvCCO+AStknMySg=="
   }
 }
 
+provider "azurerm"
+  features {}
+}
