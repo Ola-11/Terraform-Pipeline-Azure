498ccf8f51915330f5a9117d7ef968ad20f50665
Oladeinde Oluwaseun <90369121+Ola-11@users.noreply.github.com>
2024-11-06 16:04:25 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,12 +14,6 @@ terraform {
   }
   backend "azurerm" {
       resource_group_name  = "RG-Project10"
-      storage_account_name = "inwayztfstate1248310649"
-      container_name       = "inwayztfstate"
-      key                  = "d905iyQFg6g2/lExrzOT7kMiektbBXGPGtwKRHwYI6qw04/qFYasGKxYFSWtpRwj7ix+b0eBvCCO+AStknMySg=="
+  
   }
-}
-
-provider "azurerm"
-  features {}
 }
